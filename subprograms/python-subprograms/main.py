d900c57acf3090aa82b019025b0572dc26140cd8
Al Jorome <c22-o888-435@uphsl.edu.ph>
2025-10-10 00:47:50 +0800

    add the main file for showcasing the abstraction

---
+++ subprograms/python-subprograms/main.py
@@ -0,0 +1,8 @@
+from prime_checking import is_prime
+
+number = int(input("Enter a number: "))
+
+if is_prime(number):
+    print(f"{number} is a prime number.")
+else:
+    print(f"{number} is not a prime number.")
