1cb1e81984351c294ee068e78440328dcb9a1ce4
LarieAmimirog <amimirog@gmail.com>
2025-10-10 02:09:48 +0800

    Add input validation and string processing to anagram checker

--- subprograms/js-subprograms/anagram.js
+++ subprograms/js-subprograms/anagram.js
@@ -8,11 +8,29 @@
  */
 
 /**
- * Basic function structure for checking anagrams
- * Will be developed incrementally
+ * Checks if two strings are anagrams of each other.
+ * The function is case-insensitive and ignores spaces and punctuation.
+ * 
+ * @param {string} str1 - First string to compare
+ * @param {string} str2 - Second string to compare
+ * @return {boolean} True if the strings are anagrams, false otherwise
  */
 function isAnagram(str1, str2) {
-    // Initial implementation - will be expanded
+    // Input validation
+    if (typeof str1 !== 'string' || typeof str2 !== 'string') {
+        throw new Error('Both inputs must be strings');
+    }
+    
+    // Process strings: convert to lowercase and remove non-alphanumeric characters
+    const cleanStr1 = str1.toLowerCase().replace(/[^a-z0-9]/g, '');
+    const cleanStr2 = str2.toLowerCase().replace(/[^a-z0-9]/g, '');
+    
+    // Quick check: anagrams must have the same length
+    if (cleanStr1.length !== cleanStr2.length) {
+        return false;
+    }
+    
+    // Character frequency counting will be added in the next commit
     return false;
 }
 
