5fed3b5249c3a5d5c63a4f3f55b11cef29dc5cc8
LarieAmimirog <amimirog@gmail.com>
2025-10-10 02:02:06 +0800

    Create initial anagram checker function structure

---
+++ subprograms/js-subprograms/anagram.js
@@ -0,0 +1,20 @@
+/**
+ * Anagram Checker Module
+ * Author: Larie Amimiorg
+ * 
+ * This module provides a function to check if two strings are anagrams.
+ * An anagram is a word or phrase formed by rearranging the letters of a different
+ * word or phrase, using all the original letters exactly once.
+ */
+
+/**
+ * Basic function structure for checking anagrams
+ * Will be developed incrementally
+ */
+function isAnagram(str1, str2) {
+    // Initial implementation - will be expanded
+    return false;
+}
+
+// Export the anagram checker function
+module.exports = isAnagram;
