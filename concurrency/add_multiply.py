fb3b1e6c4c42f06cd2fa1e7d91fd3a06106f17fc
Agatha Floreta <agathafloreta1@gmail.com>
2025-10-16 22:46:56 +0800

    feat: add addition and multiplication task functions - task creation

--- concurrency/add_multiply.py
+++ concurrency/add_multiply.py
@@ -5,6 +5,16 @@ from datetime import datetime
 def timestamp():
     return datetime.now().strftime("%H:%M:%S")
 
+def addition(first_num, second_num):
+    print(f"[{timestamp()}] ADDING: {first_num} + {second_num}")
+    time.sleep(3)  # Simulate time-consuming calculation
+    print(f"[{timestamp()}] SUM: {first_num + second_num}")
+
+def multiplication(first_num, second_num):
+    print(f"[{timestamp()}] MULTIPLYING: {first_num} * {second_num}")
+    time.sleep(3)  # Simulate time-consuming calculation
+    print(f"[{timestamp()}] PRODUCT: {first_num * second_num}")
+
 if __name__ == "__main__":
     # Get input from user
     first_num = int(input("Enter the first number: "))
